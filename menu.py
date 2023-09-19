35f7ff86a8e0f80f5983a3fddc53aecb691d8a5b
Lara <92634014+LaraChel@users.noreply.github.com>
2023-09-19 20:58:04 +0300

    v 1.0
    1 версия

---
+++ menu.py
@@ -0,0 +1,19 @@
+import rich
+from rich.console import Console
+
+console = Console()
+
+def Menu():
+    functions = [
+        "reply in chat",
+        "reply to all chats",
+        "reply in pm",
+        "join chat",
+        "set reactions"
+    ]
+
+    for index, functions in enumerate(functions, 1):
+        console.print(
+            "([bold red]{index}[/]) {functions}"
+            .format(index=index, functions=functions)
+        )
